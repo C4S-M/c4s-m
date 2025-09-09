4b8ea815c132e64dfcb5e7c87dea64d4709c85f3
C4S-M <cmatthys6@gmail.com>
2025-09-09 21:13:37 +0200

    Create background.js

---
+++ background.js
@@ -0,0 +1,16 @@
+        // Vanta.js 3D achtergrond initialisatie
+        VANTA.WAVES({
+          el: "#vanta-bg",
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          color: 0x5a5a5a, // Een grijze kleur voor een "chrome"-achtig gevoel
+          shininess: 50.00, // Verhoogd voor een meer metaalachtige glans
+          waveHeight: 15.00,
+          waveSpeed: 0.50,
+          zoom: 0.75
+        });
